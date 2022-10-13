b88661faaaab07c3d2e00efee215627f48928cf0
42mst <mstriek@42mst.tech>
2022-10-13 15:40:44 +0200

    9. Palindrome Number

---
+++ Java/src/PalindromeNumber.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Palindrome_Number {
+    public boolean isPalindrome(int x) {
+        
+        int remainder, sum = 0, temp; 
+
+        if (x < 0){
+            x = (-x);
+        }
+
+        if((x >= 0) && (x < 10)){
+            return true;
+        }
+        
+        temp = x;
+        while(x > 0){
+            remainder = x % 10;  //getting remainder  
+            sum = (sum * 10) + remainder;    // add remainder each iteration
+            x = x / 10;   // cut the last digit to continue
+        }
+
+        if (temp == sum) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int num = sc.nextInt();
+
+            Palindrome_Number s = new Palindrome_Number();
+            if(s.isPalindrome(num)){
+                System.out.println("True");
+            } else {
+                System.out.println("False");
+            }
+        }
+    }
+}
